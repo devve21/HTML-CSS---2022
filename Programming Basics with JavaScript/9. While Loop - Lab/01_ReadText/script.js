8031f56ecde881117de0202d44098ff503d7bb80
Anton Stoyanov <77509796+devve21@users.noreply.github.com>
2022-05-30 21:09:46 +0300

    Add files via upload

---
+++ Programming Basics with JavaScript/9. While Loop - Lab/01_ReadText/script.js
@@ -0,0 +1,16 @@
+function readText (input) {
+    let i = 0;
+    let word = input[i];
+    
+    
+    while(word !== "Stop"){
+       
+       console.log(word);
+      
+       i++
+       word = input[i];
+       
+    }
+
+
+}
---
+++ Programming Basics with JavaScript/9. While Loop - Lab/04_ Sequence2k+1/script.js
@@ -0,0 +1,12 @@
+function sequence2k2(input){
+
+let n = Number(input[0]);
+
+let index = 1;
+
+while(index <= n){
+    console.log(index);
+   index = (index * 2) + 1;
+}
+
+}
