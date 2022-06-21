104893bdbf21927711672426e20f97d99c98dfce
Anton Stoyanov <77509796+devve21@users.noreply.github.com>
2022-06-22 01:53:53 +0300

    Add files via upload

---
+++ Programming Fundamentals with JavaScript/2. Basic Syntax, Conditional Statements and Loops - Exercise/08_MultiplicationTable/multiplicationTable.js
@@ -0,0 +1,8 @@
+function multiplicationTable(num) {
+
+    for (let currentNumber = 1; currentNumber <= 10; currentNumber++) {
+        console.log(`${num} X ${currentNumber} = ${num * currentNumber}`);
+    }
+
+}
+multiplicationTable(5);
